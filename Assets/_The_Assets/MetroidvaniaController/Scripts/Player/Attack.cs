6d938e168b773ee271e39c1714a80587988c08da
DESKTOP-KH7DB9P\matha <techchintu2017@gmail.com>
2023-01-11 14:15:25 -0800

    attempting player projectile fix

--- Assets/_The_Assets/MetroidvaniaController/Scripts/Player/Attack.cs
+++ Assets/_The_Assets/MetroidvaniaController/Scripts/Player/Attack.cs
@@ -38,7 +38,8 @@ public class Attack : MonoBehaviour
 
 		if (Input.GetKeyDown(KeyCode.Mouse1))
 		{
-			GameObject throwableWeapon = Instantiate(throwableObject, transform.position + new Vector3(transform.localScale.x * 0.5f,-0.2f), Quaternion.identity) as GameObject; 
+			//GameObject throwableWeapon = Instantiate(throwableObject, transform.position + new Vector3(transform.localScale.x * 0.5f,-0.2f), Quaternion.identity) as GameObject; 
+			GameObject throwableWeapon = Instantiate(throwableObject, attackCheck.position, attackCheck.rotation);
 			Vector2 direction = new Vector2(transform.localScale.x, 0);
 			throwableWeapon.GetComponent<ThrowableWeapon>().direction = direction; 
 			throwableWeapon.name = "ThrowableWeapon";
