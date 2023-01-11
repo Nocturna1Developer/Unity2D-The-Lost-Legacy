e680511864b2de79e8fb3b1b81336d99d9fb5619
DESKTOP-KH7DB9P\matha <techchintu2017@gmail.com>
2023-01-10 20:20:32 -0800

    Checkpoint - screen flashes done

--- Assets/_Scripts/Item Scripts/CollideManager.cs
+++ Assets/_Scripts/Item Scripts/CollideManager.cs
@@ -16,6 +16,9 @@ public class CollideManager : MonoBehaviour, IGetHealthSystem
     [SerializeField] private GameObject _playerBody;
     //[SerializeField] private PlayerController _playerController;
     [SerializeField] private RectTransform _faderImage;
+    [SerializeField] private RectTransform _collectImage;
+    [SerializeField] private RectTransform _damageImage;
+    [SerializeField] private RectTransform _healImage;
 
     [Header("Camera Properties")]
     [SerializeField] private ShakeData _screenShakeData = null;
@@ -41,16 +44,36 @@ public class CollideManager : MonoBehaviour, IGetHealthSystem
         healthSystem = new HealthSystem(100);
 
         healthSystem.OnDead += HealthSystem_OnDead;
-        healthSystem.OnDamaged += HealthSystem_OnDamaged;
-        healthSystem.OnHealed += HealthSystem_OnHealed;
+        //healthSystem.OnDamaged += HealthSystem_OnDamaged;
+        //healthSystem.OnHealed += HealthSystem_OnHealed;
     }
 
     private void Start()
     {
+        // Screen flashes when interacting with objects in scene
         _faderImage.gameObject.SetActive(true);
-        LeanTween.alpha(_faderImage, 1, 0);
-        LeanTween.alpha(_faderImage, 0, 2f).setOnComplete(() => {
+        _collectImage.gameObject.SetActive(true);
+        _damageImage.gameObject.SetActive(true);
+        _healImage.gameObject.SetActive(true);
+
+        LeanTween.scale (_faderImage, new Vector3 (1, 1, 1), 0);
+        LeanTween.scale (_faderImage, Vector3.zero, 1f).setEase (LeanTweenType.easeInOutQuad).setOnComplete (() => {
             _faderImage.gameObject.SetActive(false);
+        });
+        
+        LeanTween.alpha (_collectImage, 0, 0);
+        LeanTween.alpha (_collectImage, 0, 0.5f).setOnComplete (() => {
+            _collectImage.gameObject.SetActive (false);
+        });
+
+        LeanTween.alpha (_damageImage, 0, 0);
+        LeanTween.alpha (_damageImage, 0, 0.5f).setOnComplete (() => {
+            _damageImage.gameObject.SetActive (false);
+        });
+
+        LeanTween.alpha (_healImage, 0, 0);
+        LeanTween.alpha (_healImage, 0, 0.5f).setOnComplete (() => {
+            _healImage.gameObject.SetActive (false);
         });
     }
 
@@ -61,23 +84,36 @@ public class CollideManager : MonoBehaviour, IGetHealthSystem
 
     private void HealthSystem_OnDead(object sender, System.EventArgs e) 
     {
-        _deathSound.Play();
-        _playerBody.SetActive(false);
-        //_playerController.enabled = false;
-        StartCoroutine(RestartLevelAfterDelay(0.3f));
-        _damageParticleSystem.Play();
-
-        _screenShakeInstance = CameraShakerHandler.Shake(_screenShakeData);
+        _faderImage.gameObject.SetActive (true);
+        LeanTween.scale (_faderImage, Vector3.zero, 0f);
+        LeanTween.scale (_faderImage, new Vector3 (1, 1, 1), 0.5f).setEase (LeanTweenType.easeInOutQuad).setOnComplete (() => {
+            _deathSound.Play();
+            _playerBody.SetActive(false);
+            StartCoroutine(RestartLevelAfterDelay(0.3f));
+            _damageParticleSystem.Play();
+            _screenShakeInstance = CameraShakerHandler.Shake(_screenShakeData);
+        });
     }
 
-    private void HealthSystem_OnDamaged(object sender, System.EventArgs e)
-    {
-        _damageParticleSystem.Play();
-    }
+    // private void HealthSystem_OnDamaged(object sender, System.EventArgs e)
+    // {
+    //     //_damageParticleSystem.Play();
+    //     _damageImage.gameObject.SetActive(false);
+    //     LeanTween.alpha(_damageImage, 1, 0);
+    //     LeanTween.alpha(_damageImage, 0, 5f).setOnComplete(() => {
+    //         _damageParticleSystem.Play();
+    //     });
+    // }
 
-    private void HealthSystem_OnHealed(object sender, System.EventArgs e) {
-        _healParticleSystem.Play();
-    }
+    // private void HealthSystem_OnHealed(object sender, System.EventArgs e)
+    // {
+    //     //_healParticleSystem.Play();
+    //     _damageImage.gameObject.SetActive(false);
+    //     LeanTween.alpha(_damageImage, 1, 0);
+    //     LeanTween.alpha(_damageImage, 0, 5f).setOnComplete(() => {
+    //         _healParticleSystem.Play();
+    //     });
+    // }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -90,29 +126,52 @@ public class CollideManager : MonoBehaviour, IGetHealthSystem
         {
             case "Coin":
                 SumScore.Add(_coinValue);
-                _coinParticleSystem.Play();
                 _coinSound.Play();
+
+                // Screen flashes red
+                _collectImage.gameObject.SetActive(true);
+                LeanTween.alpha(_collectImage, 1, 0);
+                LeanTween.alpha(_collectImage, 0, 0.5f).setOnComplete(() => {
+                    _coinParticleSystem.Play();
+                });
                 break;
 
             case "Enemy":
                 _hurtSound.Play();
                 healthSystem.Damage(_enemyDamage);
-                _damageParticleSystem.Play();
                 _screenShakeInstance = CameraShakerHandler.Shake(_screenShakeData);
+
+                // Screen flashes red
+                _damageImage.gameObject.SetActive(true);
+                LeanTween.alpha(_damageImage, 1, 0);
+                LeanTween.alpha(_damageImage, 0, 0.5f).setOnComplete(() => {
+                    _damageParticleSystem.Play();
+                });
                 break;
             
             case "Fungus":
                 _hurtSound.Play();
                 healthSystem.Damage(15f);
-                _damageParticleSystem.Play();
                 _screenShakeInstance = CameraShakerHandler.Shake(_screenShakeData);
+                
+                // Screen flashes red
+                _damageImage.gameObject.SetActive(true);
+                LeanTween.alpha(_damageImage, 1, 0);
+                LeanTween.alpha(_damageImage, 0, 0.5f).setOnComplete(() => {
+                    _damageParticleSystem.Play();
+                });
                 break;
             
             case "Heal":
                 _healSound.Play();
                 healthSystem.Heal(_healAmount);
-                _healParticleSystem.Play();
-                //_screenShakeInstance = CameraShakerHandler.Shake(_screenShakeData);
+
+                // Screen flashes green
+                _healImage.gameObject.SetActive(true);
+                LeanTween.alpha(_healImage, 1, 0);
+                LeanTween.alpha(_healImage, 0, 0.5f).setOnComplete(() => {
+                    _healParticleSystem.Play();
+                });
                 break;
             
             case "Death":
